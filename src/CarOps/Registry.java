4db637647c00734c95053c30fa9bec536f4eb0c6
iis22023 <134298380+iis22023@users.noreply.github.com>
2023-05-31 21:00:08 +0300

    Update Registry.java

--- src/CarOps/Registry.java
+++ src/CarOps/Registry.java
@@ -16,6 +16,54 @@ public class Registry {
 	ArrayList<Task> Tasks = new ArrayList<Task>();
 	ArrayList<Vehicle> Vehicles = new ArrayList<Vehicle>();
 	
+	public void AddNewItem(AssignmentSparePart anAssignmentSparePart) {
+		AssignmentSpareParts.add(anAssignmentSparePart);
+	}
+	
+	public void AddNewItem(Client aClient) {
+		Clients.add(aClient);
+	}
+	
+	public void AddNewItem(Engineer anEngineer) {
+		Engineers.add(anEngineer);
+	}
+	
+	public void AddNewItem(Repair aRepair) {
+		Repairs.add(aRepair);
+	}
+	
+	public void AddNewItem(RepairFolder aRepairFolder) {
+		RepairFolders.add(aRepairFolder);
+	}
+	
+	public void AddNewItem(RepairTask aRepairTask) {
+		RepairTasks.add(aRepairTask);
+	}
+	
+	public void AddNewItem(Report aReport) {
+		Reports.add(aReport);
+	}
+	
+	public void AddNewItem(Session aSession) {
+		Sessions.add(aSession);
+	}
+	
+	public void AddNewItem(SparePart aSparePart) {
+		SpareParts.add(aSparePart);
+	}
+	
+	public void AddNewItem(TaskAssignment aTaskAssigment) {
+		TaskAssignments.add(aTaskAssigment);
+	}
+	
+	public void AddNewItem(Task aTask) {
+		Tasks.add(aTask);
+	}
+	
+	public void AddNewItem(Vehicle aVehicle) {
+		Vehicles.add(aVehicle);
+	}
+	
 	
 	
 	
