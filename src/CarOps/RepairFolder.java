577373f180ca9b81d220dc0c2f3f39e2c6a07fd5
iis22023 <134298380+iis22023@users.noreply.github.com>
2023-05-31 22:28:13 +0300

    Completed RepairFolde

--- src/CarOps/RepairFolder.java
+++ src/CarOps/RepairFolder.java
@@ -1,20 +1,35 @@
 package CarOps;
 
+import java.util.ArrayList;
+
 public class RepairFolder {
 
-	Repair AriseFrom;
+	
 	private int RepairFolderID;
 	private String status;
 	private String EstTime;
 	private int TotalCost;
+	private Registry registry;
+	ArrayList<Repair> repairs = new ArrayList<Repair>();
 
-	public RepairFolder GetFolder() {
-		// TODO - implement RepairFolder.GetFolder
-		throw new UnsupportedOperationException();
+	public void PrintDetails() {
+		System.out.println("Repair Folder Details:");
+		System.out.println("ID: "+ RepairFolderID);
+		System.out.println("Status: "+ status);
+		System.out.println("Estimated Time: "+ EstTime);
+		System.out.println("TotalCost: "+ TotalCost);
 	}
+	
 
-	public void SetEstimatedTime(String EstTime) {
-		this.EstTime = EstTime;
+	public RepairFolder(Registry registry, String status, String estTime, int totalCost) {
+		this.registry = registry;
+		RepairFolderID = registry.RepairFolders.size() + 1;
+		this.status = status;
+		EstTime = estTime;
+		TotalCost = totalCost;
 	}
-
+	
+	public void AddRepair(Repair aRepair) {
+		repairs.add(aRepair);
+	}
 }
