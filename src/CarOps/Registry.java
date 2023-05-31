c126cc457b43fe13bae638336f8cdb5b63632a63
iis22023 <134298380+iis22023@users.noreply.github.com>
2023-05-31 20:44:51 +0300

    Update class Registry
    Προσθήκη στην κλάση "Registry" όλους του καταλόγους - Αφαίρεση περιττών κλάσεων

--- src/CarOps/Registry.java
+++ src/CarOps/Registry.java
@@ -1,5 +1,23 @@
 package CarOps;
 
+import java.util.ArrayList;
+
 public class Registry {
+	ArrayList<AssignmentSparePart> AssignmentSpareParts = new ArrayList<AssignmentSparePart>();
+	ArrayList<Client> Clients = new ArrayList<Client>();
+	ArrayList<Engineer> Engineers = new ArrayList<Engineer>();
+	ArrayList<Repair> Repairs = new ArrayList<Repair>();
+	ArrayList<RepairFolder> RepairFolders = new ArrayList<RepairFolder>();
+	ArrayList<RepairTask> RepairTasks = new ArrayList<RepairTask>();
+	ArrayList<Report> Reports = new ArrayList<Report>();
+	ArrayList<Session> Sessions = new ArrayList<Session>();
+	ArrayList<SparePart> SpareParts = new ArrayList<SparePart>();
+	ArrayList<TaskAssignment> TaskAssignments = new ArrayList<TaskAssignment>();
+	ArrayList<Task> Tasks = new ArrayList<Task>();
+	ArrayList<Vehicle> Vehicles = new ArrayList<Vehicle>();
+	
+	
+	
+	
 
 }
