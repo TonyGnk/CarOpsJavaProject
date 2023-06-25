858e72655b17b5df310bc78497f4a44dc4794c56
Antonis Giannakovitis <61454221+TonyGnk@users.noreply.github.com>
2023-06-25 12:43:47 +0300

    FIX

--- src/screens/StartScr.java
+++ src/screens/StartScr.java
@@ -3,17 +3,23 @@ package screens;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class StartScr extends VBox {
     private VBox allContentBox;
     private HBox contentBox;
+    private HBox closeButtonBox; // New HBox for the close button
 
-    public StartScr(Stage primaryStage) {
+    public StartScr(Stage primaryStage) throws FileNotFoundException {
         super();
         // Create instances of user screen classes
         DefScreen gramateia = new UserSecretaryHome(primaryStage);
@@ -29,18 +35,19 @@ public class StartScr extends VBox {
         Button mhhanikosButton = new DefButtonStart("Μηχανικός", mhhanikos);
         Button mhhanikosEpilepButton = new DefButtonStart("Επιβλέπων Μηχανικός", mhhanikosEpilep);
 
-        
-        //this.setAlignment(Pos.CENTER);
-        
-        Label connectAsLabel = new DefTextHeader("Ποιος θα χρησιμοποιήσει το CarOps;");
-        
+        // Create the close button
+        DefButtonHome closeButton = new DefButtonHome("N", "Κλείσιμο");
+        closeButton.setOnAction(e -> primaryStage.close());
+
+        Label connectAsLabel = new DefTextHeader("Ποιος θα χρησιμοποιήσει το CarOps?");
+
         // Create a container for the buttons
         allContentBox = new VBox();
         allContentBox.setAlignment(Pos.CENTER);  // Align the big box in the center vertically
         allContentBox.setMargin(connectAsLabel, new Insets(40));
         allContentBox.setSpacing(80);
         allContentBox.setPadding(new Insets(10));
-        
+
         // Create a container for the buttons
         contentBox = new HBox();
         contentBox.setAlignment(Pos.CENTER);
@@ -55,10 +62,29 @@ public class StartScr extends VBox {
                 mhhanikosButton,
                 mhhanikosEpilepButton
         );
-        
+
         // Add the label and the button container to the main VBox
         allContentBox.getChildren().addAll(connectAsLabel, contentBox);
-        this.getChildren().add(allContentBox);
+
+        // Create an HBox for the close button and center it
+        closeButtonBox = new HBox(closeButton);
+        closeButtonBox.setAlignment(Pos.CENTER);
+
+        // Load the image
+        Image image = new Image(new FileInputStream("C:\\ADBKit\\user.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(200);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+
+        // Set up the layout
+        setAlignment(Pos.CENTER);
+        setSpacing(40);
+        setPadding(new Insets(10));
+
+        // Add the image view, the main VBox, and the close button to the root VBox
+        getChildren().addAll(imageView, allContentBox, closeButtonBox);
     }
 }
 
