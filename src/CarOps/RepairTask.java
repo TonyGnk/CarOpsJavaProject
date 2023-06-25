2241bf0d0beae1b8bdf58f6cff157f5d39b9fad6
iis22023 <134298380+iis22023@users.noreply.github.com>
2023-06-25 17:11:45 +0300

    Update RepairTask.java

--- src/CarOps/RepairTask.java
+++ src/CarOps/RepairTask.java
@@ -4,20 +4,36 @@ import java.util.ArrayList;
 
 public class RepairTask {
 
-	private int RepairTaskID;
+	private int repairTaskID;
 	private TaskAssignment aTaskAssignment;
-	private Task atask ;
+	private ArrayList<Task> Tasks =  new ArrayList<Task>();
 	private Registry registry;
 
-	public Task GetRepairTask(int RepairFolderID) {
-		// TODO - implement RepairTask.GetRepairTask
-		throw new UnsupportedOperationException();
+
+	public RepairTask(int repairTaskID, TaskAssignment aTaskAssignment, ArrayList<Task> tasks, Registry registry) {
+		
+		this.repairTaskID = repairTaskID;
+		this.aTaskAssignment = aTaskAssignment;
+		Tasks = tasks;
+		this.registry = registry;
 	}
 
 
-	public RepairTask(int RepairFolderID, Task aTask) {
+	public void printDetails() {
+		
+		System.out.println("ID: " +repairTaskID);
+		System.out.println("Has the following tasks:");
+		
+		int i=1;
+		for(Task aTask:Tasks) {
+			System.out.println("Task "+i+":");
+			aTask.PrintDetails();
+			i++;
+		}
+	}
+
+
 	
-	}
 	
 	/*public void addTask(Task aTask) {
 		tasks.add(aTask);
